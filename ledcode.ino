a3f1bc6289f1fe318f4c76b93c97b0d2df46ceaa
shanki01 <93405786+shanki01@users.noreply.github.com>
2023-03-09 10:23:27 -0500

    Add files via upload

---
+++ ledcode.ino
@@ -0,0 +1,16 @@
+#define LED PIN
+
+int LED_PIN = 12;
+
+void setup()
+{
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(500); 
+}
